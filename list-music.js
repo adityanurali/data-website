5f2129e1ccaf9f0862bc602f561c0ff951f6614e
adityanurali <69146268+adityanurali@users.noreply.github.com>
2022-09-21 02:00:08 +0700

    Add files via upload

---
+++ list-music.js
@@ -0,0 +1,47 @@
+//daftar musik yang akan di simpan di server
+//berhubung saya lagi miskim jadi tak simpen di github saja
+//dukung saya dong biar terus berkembang
+let allMusic = [
+  {
+    name: "arabic- zauji",
+    artist: "arabic song",
+    img: "https://user-images.githubusercontent.com/69146268/191341069-01612f18-633d-4afd-bc26-b028a4cba65e.gif",
+    src: "https://pomf2.lain.la/f/ns6g4q2c.mp3",
+  },
+  {
+    name: "najwa farouk-waylo",
+    artist: "arabic song",
+    img: "https://user-images.githubusercontent.com/69146268/191340964-7f59f1ab-89f5-4b08-8587-77a9aa975e5a.gif",
+    src: "https://pomf2.lain.la/f/u5btczpm.mp3"
+  },
+  {
+    name: "nancy ajram-enta eih",
+    artist: "arabic song",
+    img: "https://user-images.githubusercontent.com/69146268/191340777-cb21480d-a1f9-438f-995b-73eba780bd74.gif",
+    src: "https://pomf2.lain.la/f/8b195yo.mp3",
+  },
+  {
+    name: "nancy ajram-ya tab tab",
+    artist: "arabic song",
+    img: "https://user-images.githubusercontent.com/69146268/191341069-01612f18-633d-4afd-bc26-b028a4cba65e.gif",
+    src: "https://pomf2.lain.la/f/yksolfx.mp3",
+  },
+  {
+    name: "nasidaria-palestina",
+    artist: "indonesia song",
+    img: "https://user-images.githubusercontent.com/69146268/191340964-7f59f1ab-89f5-4b08-8587-77a9aa975e5a.gif",
+    src: "https://pomf2.lain.la/f/okjunjv.mp3",
+  },
+  {
+    name: "nada sikkah-ghanili",
+    artist: "arabic somg",
+    img: "https://user-images.githubusercontent.com/69146268/191340777-cb21480d-a1f9-438f-995b-73eba780bd74.gif",
+    src: "https://pomf2.lain.la/f/xrt5daa0.mp3",
+  },
+  {
+    name: "nada sikkah-tasmauni robbah",
+    artist: "arabic song",
+    img: "https://user-images.githubusercontent.com/69146268/191341069-01612f18-633d-4afd-bc26-b028a4cba65e.gif",
+    src: "https://pomf2.lain.la/f/u6g1q72p.mp3",
+  },
+];
