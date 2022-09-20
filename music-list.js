2bdae2e43cbe67f75019c5466dc2bfbcc6561e44
adityanurali <69146268+adityanurali@users.noreply.github.com>
2022-09-20 15:40:01 +0700

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,65 @@
+//daftar musik yang akan di simpan di server
+//berhubung saya lagi miskim jadi tak simpen di github saja
+//dukung saya dong biar terus berkembang
+let allMusic = [
+  {
+    name: "arabic- zaujati",
+    artist: "mbuh ora kenal",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "pengembangan",
+    artist: "pengembangan",
+    img: "musik",
+    src: "musik"
+  },
+  {
+    name: "pengembangan",
+    artist: "pengembangan",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "pengembangan",
+    artist: "pemgembangan",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Jim - Sun Goes Down",
+    artist: "Jim Yosef x Roy",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Lost Sky - Vision NCS",
+    artist: "NCS Release",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "masih kosong",
+    artist: "kosong",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "masih kosong1",
+    artist: "kosong1",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+  	name: "masih kosong2",
+  artist: "masih kosong2",
+  img: "musik2",
+  src: "musik2",
+  },
+  {
+    name: "masih kosong3",
+    artist: "masih kosong3",
+    img: "masih kosong kosong3",
+    src: "masih kosong3",
+  },
+];
